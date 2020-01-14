5e66e079d9a254955b23e2b2a0f5132952f1837e
ferestgo <ferestgo@gmail.com>
2020-01-14 18:47:56 +0300

    update temlate for migration file

--- create.go
+++ create.go
@@ -12,8 +12,8 @@ const timeFormat = "20060102150405"
 var template = `package main
 
 import (
+	migrations "github.com/whenspeakteam/go-pg-migrations/v2"
 	"github.com/whenspeakteam/pg/v9/orm"
-	migrations "github.com/robinjoseph08/whenspeakteam-migrations/v2"
 )
 
 func init() {
